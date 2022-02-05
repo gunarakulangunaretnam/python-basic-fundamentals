ddbacdea0723ea293b839a76488d57fb3457dbae
GunaRakulan-GR <gunarakulan@gmail.com>
2022-02-05 23:53:36 +0530

    Add nested-dictionary code

---
+++ 30-nested-dictionary-in-python.py
@@ -0,0 +1,10 @@
+employee = {
+
+"employee_1":{"name":"David", "age":45, "job":"Engineer"},
+"employee_2":{"name":"Frank", "age":32, "job":"Web Developer"},
+"employee_3":{"name":"Guna", "age":24, "job":"Engineer"}
+
+}
+
+
+print(employee["employee_2"]["job"])
